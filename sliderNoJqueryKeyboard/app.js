0d68a9d11853601d7e8c9bc8908f25672c8cca3c
Romain Quilliot <romain.addweb@gmail.com>
2015-03-30 19:32:34 +0200

    Add sliderNoJQueryKeyboard

---
+++ sliderNoJqueryKeyboard/app.js
@@ -0,0 +1,100 @@
+/**
+ * Methode executee lors du clique sur NEXT
+ *
+ */
+function nextCallback () {
+    var nextSlide = currentSlide + 1;
+    
+    // Si la prochaine slide n'existe pas
+    if (outOfRange(nextSlide)) return false;
+
+    // On check les boutons
+    verifyButtons(nextSlide);
+    
+    elements[currentSlide].style.display = "none";
+    elements[nextSlide].style.display = "block";
+    currentSlide = nextSlide;
+    
+}
+
+/**
+ * Methode executee lors du click sur PREV
+ *
+ */
+function previousCallback() {
+    var prevSlide = currentSlide - 1;
+
+    // Si la prochaine slide n'existe pas
+    if (outOfRange(prevSlide)) return false;
+    
+    // On check les boutons
+    verifyButtons(prevSlide);
+    
+    elements[currentSlide].style.display = "none";
+    elements[prevSlide].style.display = "block";
+    currentSlide = prevSlide;
+}
+
+/**
+ * Methode executee lors du clique sur les touches du clavier
+ *
+ */
+function keyCallback(e) {
+    switch (e.keyCode) {
+        case 37:
+            previousCallback();
+            break;
+        case 39:
+            nextCallback();
+            break;
+    }
+}
+
+/**
+ * Methode pour verifier si les boutons
+ * doivent etre caches ou affiches
+ * @param int slide Prochain slide affiche
+ *
+ */
+function verifyButtons(slide) {
+    if (slide >= (elements.length - 1)) {
+        next.style.display = "none";
+    } else {
+        next.style.display = "inline";
+    }
+    
+    if (slide <= 0) {
+        previous.style.display = "none";
+    } else {
+        previous.style.display = "inline";
+    }
+}
+
+/**
+ * Methode pour verifie que la slide existe
+ * @param int slide
+ * @return bool
+ *
+ */
+function outOfRange(slide) {
+    if (slide < 0  || slide > (elements.length - 1)) {
+        return true;
+    }
+
+    return false;
+}
+
+
+/** INIT **/
+var elements = document.querySelectorAll('.slide');
+var next = document.querySelector('#next');
+var previous = document.querySelector('#previous');
+var currentSlide = 0;
+
+elements[currentSlide].style.display = "block";
+verifyButtons(currentSlide);
+
+/** LISTENER **/
+next.addEventListener('click', nextCallback);
+previous.addEventListener('click', previousCallback);
+document.addEventListener('keyup', keyCallback);
